6757f1eabfa723d12a3169d263d4c88a62c9e4e9
Max Coppen <44031065+mxcop@users.noreply.github.com>
2022-12-08 16:46:16 +0100

    refactor: lexer cleanup

--- compiler/wax_lexer/src/char.rs
+++ compiler/wax_lexer/src/char.rs
@@ -1,18 +1,17 @@
-/** Is this character the start of a new identity? */
-pub fn is_ident_start(ch: char) -> bool {
-  'a' <= ch && ch <= 'z' || 
-  'A' <= ch && ch <= 'Z'
+/** Is this character part of an identity? */
+pub fn is_ident(is_first: bool, ch: &char) -> bool {
+  if is_first {
+    'a' <= *ch && *ch <= 'z' || 
+    'A' <= *ch && *ch <= 'Z'
+  } else {
+    'a' <= *ch && *ch <= 'z' || 
+    'A' <= *ch && *ch <= 'Z' || 
+    '0' <= *ch && *ch <= '9' ||
+
+    *ch == '_' || *ch == '-'
+  }
 }
 
-/** Is this character still part of the identity? */
-pub fn is_ident(ch: char) -> bool {
-  'a' <= ch && ch <= 'z' || 
-  'A' <= ch && ch <= 'Z' || 
-  '0' <= ch && ch <= '9' ||
-
-  ch == '_' || ch == '-'
-}
-
-pub fn is_number(ch: char) -> bool {
-  '0' <= ch && ch <= '9' || ch == '.'
+pub fn is_number(_: bool, ch: &char) -> bool {
+  '0' <= *ch && *ch <= '9' || *ch == '.'
 }
--- compiler/wax_lexer/src/lib.rs
+++ compiler/wax_lexer/src/lib.rs
@@ -3,7 +3,7 @@ pub mod span;
 pub mod iter;
 mod char;
 
-use self::char::{is_ident_start, is_ident, is_number};
+use self::char::{is_ident, is_number};
 use iter::TrackingIter;
 use token::{Token, SyntaxToken};
 
@@ -38,33 +38,21 @@ impl<'a> Lexer<'a> {
     space.len()
   }
 
-  /// ### Read next number
-  fn number(&mut self, first_ch: char) -> Option<String> {
+  /// Read the next `n` amount of chars that all match the predicate.<br>
+  /// ```
+  /// predicate = fn(is_first: bool, ch: &char) -> bool
+  /// ```
+  fn read_next(&mut self, 
+    first_ch: &char,
+    predicate: fn(bool, &char) -> bool) 
+  -> Option<String> {
+
     let mut word: Vec<char> = Vec::new();
 
-    if is_number(first_ch) {
-      word.push(first_ch);
+    if predicate(true, first_ch) {
+      word.push(*first_ch);
       while let Some(&ch) = self.iter.peek() {
-        if is_number(*ch) {
-          self.next();
-          word.push(*ch);
-        } else {
-          return Some(word.iter().collect());
-        }
-      }
-    }
-
-    None
-  }
-
-  /// ### Read next identity
-  fn ident(&mut self, first_ch: char) -> Option<String> {
-    let mut word: Vec<char> = Vec::new();
-
-    if is_ident_start(first_ch) {
-      word.push(first_ch);
-      while let Some(&ch) = self.iter.peek() {
-        if is_ident(*ch) {
+        if predicate(false, ch) {
           self.next();
           word.push(*ch);
         } else {
@@ -97,88 +85,12 @@ impl<'a> Lexer<'a> {
     // Move through all the characters:
     while let Some(&ch) = self.next() {
 
-      // Handle spaces.
-      if ch == ' ' {
-        let len = self.whitespace(ch);
-        tokens.push(SyntaxToken::new(
-          Token::Whitespace(len), 
-          self.iter.current_pos(), len
-        ));
+      let Some(token) = self.evaluate(&ch) else {
         continue;
-      }
-
-      // Handle newlines.
-      if ch == '\n' {
-        tokens.push(SyntaxToken::new(
-          Token::Newline, 
-          self.iter.current_pos(), 1
-        ));
-        continue;
-      }
-
-      // Html part of the file:
-      let token: Token = match ch {
-        // Systematic
-        ' ' => Token::Whitespace(self.whitespace(ch)),
-        '\n' => Token::Newline,
-        '\r' => continue,
-
-        // Generic
-        ',' => Token::Comma,
-        '.' => Token::Dot,
-        '\'' => Token::SingleQuote,
-        '"' => Token::DoubleQuote,
-        '`' => Token::Grave,
-        ':' => Token::Colon,
-        ';' => Token::Semicolon,
-
-        '+' => Token::Plus,
-        '-' => { if self.cmove('>') { Token::RightArrow } else { Token::Minus } },
-        '=' => Token::Equals,
-        '*' => Token::Star,
-
-        '#' => Token::Hash,
-        '%' => Token::Percent,
-        '&' => Token::Ampersand,
-        '@' => Token::Atsign,
-        '$' => Token::Dollarsign,
-        '~' => Token::Tilde,
-        '/' => Token::Slash,
-        '\\' => Token::BackSlash,
-        '!' => Token::Bang,
-        '?' => Token::Quest,
-
-        '<' => { if self.cmove('-') { Token::LeftArrow } else { Token::LessThen } },
-        '>' => Token::GreaterThen,
-        '(' => Token::LeftParenthesis,
-        ')' => Token::RightParenthesis,
-        '{' => Token::LeftCurlyBracket,
-        '}' => Token::RightCurlyBracket,
-        '[' => Token::LeftSquareBracket,
-        ']' => Token::RightSquareBracket,
-
-        _ => {
-          // Read the next word as an identity:
-          if let Some(identity) = self.ident(ch) {
-            if let Some(keyword) = Token::from_string(&identity) {
-              keyword // Found keyword.
-            } else {
-              Token::Ident(identity)
-            }
-          } else if let Some(number) = self.number(ch) {
-            Token::Number(number)
-          } else {
-            // If the char wasn't matched and isn't an identity, it's illegal.
-            Token::Illegal(ch)
-          }
-        }
       };
 
       // Push the token onto the stack.
-      tokens.push(SyntaxToken::new(
-        token.clone(),
-        self.iter.current_pos(), token.to_string().len()
-      ));
+      tokens.push(token);
     }
 
     // End of File reached.
@@ -187,5 +99,120 @@ impl<'a> Lexer<'a> {
       self.iter.current_pos() - 1, 0
     ));
     tokens
+  }
+
+  /// Evaluate the next character.
+  fn evaluate(&mut self, ch: &char) -> Option<SyntaxToken> {
+    /* Spaces */
+    if *ch == ' ' {
+      let len = self.whitespace(*ch);
+      return Some(SyntaxToken::new(
+        Token::Whitespace(len), 
+        self.iter.current_pos(), len
+      ));
+    }
+
+    /* Newlines */
+    if *ch == '\n' {
+      return Some(SyntaxToken::new(
+        Token::Newline, 
+        self.iter.current_pos(), 1
+      ));
+    }
+
+    // Evaluate which token this char is:
+    let Some(token) = self.token_from_char(ch) else {
+      return None;
+    };
+
+    // Return the evaluated token:
+    Some(SyntaxToken::new(
+      token.clone(),
+      self.iter.current_pos(), token.to_string().len()
+    ))
+  }
+
+  /// Evaluate which token a char should become.
+  fn token_from_char(&mut self, ch: &char) -> Option<Token> {
+
+    // Ignore return chars:
+    if *ch == '\r' {
+      return None;
+    }
+
+    let token = match ch {
+      /* Systematic */
+      ' ' => Token::Whitespace(self.whitespace(*ch)),
+      '\n' => Token::Newline,
+
+      /* Generic */
+      ',' => Token::Comma,
+      '.' => Token::Dot,
+      '\'' => Token::SingleQuote,
+      '"' => Token::DoubleQuote,
+      '`' => Token::Grave,
+      ':' => Token::Colon,
+      ';' => Token::Semicolon,
+
+      /* Math Symbols */
+      '+' => Token::Plus,
+      '-' => { if self.cmove('>') { Token::RightArrow } else { Token::Minus } },
+      '=' => Token::Equals,
+      '*' => Token::Star,
+      '/' => Token::Slash,
+
+      /* Special Symbols */
+      '#' => Token::Hash,
+      '%' => Token::Percent,
+      '&' => Token::Ampersand,
+      '@' => Token::Atsign,
+      '$' => Token::Dollarsign,
+      '~' => Token::Tilde,
+      '\\' => Token::BackSlash,
+      '!' => Token::Bang,
+      '?' => Token::Quest,
+
+      /* Closures */
+      '<' => { if self.cmove('-') { Token::LeftArrow } else { Token::LessThen } },
+      '>' => Token::GreaterThen,
+      '(' => Token::LeftParenthesis,
+      ')' => Token::RightParenthesis,
+      '{' => Token::LeftCurlyBracket,
+      '}' => Token::RightCurlyBracket,
+      '[' => Token::LeftSquareBracket,
+      ']' => Token::RightSquareBracket,
+
+      /* Words */
+      _ => {
+        self.token_from_ident(ch)
+      }
+    };
+
+    Some(token)
+  }
+
+  /// Evaluate which token an identity should be.
+  fn token_from_ident(&mut self, first_ch: &char) -> Token {
+    /* Identity [a-zA-Z] */
+    if let Some(identity) 
+      = self.read_next(first_ch, is_ident) 
+    {
+      /* Keywords */
+      if let Some(keyword) = Token::from_string(&identity) {
+        keyword
+      } else {
+        Token::Ident(identity)
+      }
+    } 
+    /* Numbers [0-9] */
+    else if let Some(number) 
+      = self.read_next(first_ch, is_number) 
+    {
+      Token::Number(number)
+    } 
+    else {
+      // If the char wasn't matched and isn't an identity, it's illegal.
+      Token::Illegal(*first_ch)
+    }
   }
 }
